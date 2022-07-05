a86ef94aa1970137f5a709abcff52c5029b43540
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-05 19:30:26 +0200

    Create frogs_constants.h

---
+++ src/frogs_constants.h
@@ -0,0 +1,14 @@
+#ifndef _FROGS_CONSTS_H
+#define _FROGS_CONSTS_H
+
+namespace frogs
+{
+
+using Integer = unsigned long long;
+using Real = long double;
+
+constexpr Real PI = 3.1415926535;
+
+} // namespace frogs
+
+#endif // _FROGS_CONSTS_H
