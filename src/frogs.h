ed36bb32c6eabd52d23cb74268c84f0be75e6681
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-08 13:47:36 +0200

    Update frogs.h

--- src/frogs.h
+++ src/frogs.h
@@ -1,9 +1,10 @@
 #ifndef _FROGS_H
 #define _FROGS_H
 
-#include "frogs_expressions.h"
+#include "frogs_primitives.h"
 #include "frogs_physical_types.h"
 #include "frogs_angle.h"
+#include "frogs_expressions.h"
 #include "frogs_diff.h"
 
 #endif // _FROGS_H
