df9d9a2992c20870ae350c694fc3732c01c2b79b
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-08 13:46:51 +0200

    Update frogs_constants.h

--- src/frogs_constants.h
+++ src/frogs_constants.h
@@ -1,11 +1,10 @@
 #ifndef _FROGS_CONSTS_H
 #define _FROGS_CONSTS_H
 
+#include "frogs_primitives.h"
+
 namespace frogs
 {
-
-using Integer = unsigned long long;
-using Real = long double;
 
 constexpr Real PI = 3.1415926535;
 
