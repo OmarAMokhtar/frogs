36168fbc8b0e818b3dc4e4b6d5a0541bcbe46b81
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-08 13:46:10 +0200

    Update frogs_expressions.h

--- src/frogs_expressions.h
+++ src/frogs_expressions.h
@@ -2,21 +2,14 @@
 #define _FROGS_EXPRESSIONS_H
 
 #include <string>
+#include <math.h>
+
+#include "frogs_primitives.h"
 
 namespace frogs
 {
 
-using Integer = unsigned long long;
-using Real = long double;
-using str = std::string;
-
 class DummyClass {};
-
-template<class T> str cov2str(T&& v) { return v.toString(); }
-inline str cov2str(double v) { return std::to_string(v); }
-inline str cov2str(Real v) { return std::to_string(v); }
-inline str cov2str(Integer v) { return std::to_string(v); }
-inline str cov2str(int v) { return std::to_string(v); }
 
 template<typename T, class Dummy = DummyClass>
 class Const
@@ -27,7 +20,23 @@ protected:
 public:
     constexpr Const(T v) : m_val{v} {}
     constexpr T val() { return m_val; }
-    str toString() { return cov2str(m_val); }
+    str toString()
+    {
+        str s = "Const{";
+        s += cov2str(m_val);
+        s += "}";
+        return s;
+    }
+};
+
+template<typename T, class Dummy = DummyClass>
+class ZeroExp
+{
+public:
+    constexpr ZeroExp() {}
+    constexpr ZeroExp(T v) {}
+    constexpr T val() { return ZeroExp(T{}); }
+    str toString() { return "0"; }
 };
 
 extern Integer ___numVars;
@@ -53,6 +62,13 @@ public:
     Var<T>& operator-=(T v)     { m_val -= v; return *this; }
     Var<T>& operator*=(Real v)  { m_val *= v; return *this; }
     Var<T>& operator/=(Real v)  { m_val /= v; return *this; }
+    
+    bool operator<(T v)  { return m_val < v; }
+    bool operator>(T v)  { return m_val > v; }
+    bool operator<=(T v) { return m_val <= v; }
+    bool operator>=(T v) { return m_val >= v; }
+    bool operator==(T v) { return m_val == v; }
+    bool operator!=(T v) { return m_val != v; }
 
     constexpr T val() { return m_val; }
     str toString()
@@ -68,232 +84,143 @@ public:
  */
 
 #define DECL_OPR_CLASS_1(ClassName, Func, Str1, Str2) \
-template<class Exp, class Dummy = DummyClass> \
-class ClassName##P { \
+template<typename Exp, class Dummy = DummyClass> \
+class ClassName { \
 protected: \
-    Exp* m_a; \
+    Exp m_a; \
 public: \
-    constexpr ClassName##P(Exp* a) : m_a{a} {} \
-    constexpr auto val() { return Func(m_a->val()); } \
-    template<typename T = ClassName##P> \
+    constexpr ClassName(Exp a) : m_a{a} {} \
+    constexpr auto val() { return Func($(m_a)); } \
     str toString() { \
         str s{Str1}; \
-        s += m_a->toString(); \
+        s += cov2str(m_a); \
         s += Str2; \
         return s; \
     } \
-    friend std::ostream &operator<<(std::ostream &output, ClassName##P& obj) { \
+    friend std::ostream &operator<<(std::ostream &output, ClassName& obj) { \
         output << obj.toString(); \
         return output; \
     } \
     constexpr Exp* arg() { return m_a; } \
 }; \
-template<class Exp, class Dummy = DummyClass> \
-class ClassName##V { \
-protected: \
-    Exp m_a; \
-public: \
-    constexpr ClassName##V(Exp a) : m_a{a} {} \
-    constexpr auto val() { return Func(m_a.val()); } \
-    str toString() { \
-        str s{Str1}; \
-        s += m_a.toString(); \
-        s += Str2; \
-        return s; \
-    } \
-    friend std::ostream &operator<<(std::ostream &output, ClassName##V& obj) { \
-        output << obj.toString(); \
-        return output; \
-    } \
-    constexpr Exp arg() { return m_a; } \
-}; \
 
 #define DECL_OPR_CLASS_2(ClassName, Func, Str1, Str2, Str3) \
 template<class Exp0, class Exp1> \
-class ClassName##VP { \
-protected: \
-    Exp0 m_a; \
-    Exp1* m_b; \
-public: \
-    constexpr ClassName##VP(Exp0 a, Exp1* b) : m_a{a}, m_b{b} {} \
-    constexpr auto val() { return Func(m_a.val(), m_b->val()); } \
-    str toString() { \
-        str s{Str1}; \
-        s += m_a.toString(); \
-        s += Str2; \
-        s += m_b->toString(); \
-        s += Str3; \
-        return s; \
-    } \
-    friend std::ostream &operator<<(std::ostream &output, ClassName##VP& obj) { \
-        output << obj.toString(); \
-        return output; \
-    } \
-    constexpr Exp0 first() { return m_a; } \
-    constexpr Exp1* second() { return m_b; } \
-}; \
-template<class Exp0, class Exp1> \
-class ClassName##PV { \
-protected: \
-    Exp0* m_a; \
-    Exp1 m_b; \
-public: \
-    constexpr ClassName##PV(Exp0* a, Exp1 b) : m_a{a}, m_b{b} {} \
-    constexpr auto val() { return Func(m_a->val(), m_b.val()); } \
-    str toString() { \
-        str s{Str1}; \
-        s += m_a->toString(); \
-        s += Str2; \
-        s += m_b.toString(); \
-        s += Str3; \
-        return s; \
-    } \
-    friend std::ostream &operator<<(std::ostream &output, ClassName##PV& obj) { \
-        output << obj.toString(); \
-        return output; \
-    } \
-    constexpr Exp0* first() { return m_a; } \
-    constexpr Exp1 second() { return m_b; } \
-}; \
-template<class Exp0, class Exp1> \
-class ClassName##VV { \
+class ClassName { \
 protected: \
     Exp0 m_a; \
     Exp1 m_b; \
 public: \
-    constexpr ClassName##VV(Exp0 a, Exp1 b) : m_a{a}, m_b{b} {} \
-    constexpr auto val() { return Func(m_a.val(), m_b.val()); } \
+    constexpr ClassName(Exp0 a, Exp1 b) : m_a{a}, m_b{b} {} \
+    constexpr auto val() { return Func($(m_a), $(m_b)); } \
     str toString() { \
         str s{Str1}; \
-        s += m_a.toString(); \
+        s += cov2str(m_a); \
         s += Str2; \
-        s += m_b.toString(); \
+        s += cov2str(m_b); \
         s += Str3; \
         return s; \
     } \
-    friend std::ostream &operator<<(std::ostream &output, ClassName##VV& obj) { \
+    friend std::ostream &operator<<(std::ostream &output, ClassName& obj) { \
         output << obj.toString(); \
         return output; \
     } \
     constexpr Exp0 first() { return m_a; } \
     constexpr Exp1 second() { return m_b; } \
-}; \
-template<class Exp0, class Exp1> \
-class ClassName##PP { \
-protected: \
-    Exp0* m_a; \
-    Exp1* m_b; \
-public: \
-    constexpr ClassName##PP(Exp0* a, Exp1* b) : m_a{a}, m_b{b} {} \
-    constexpr auto val() { return Func(m_a->val(), m_b->val()); } \
-    str toString() { \
-        str s{Str1}; \
-        s += m_a->toString(); \
-        s += Str2; \
-        s += m_b->toString(); \
-        s += Str3; \
-        return s; \
-    } \
-    friend std::ostream &operator<<(std::ostream &output, ClassName##PP& obj) { \
-        output << obj.toString(); \
-        return output; \
-    } \
-    constexpr Exp0* first() { return m_a; } \
-    constexpr Exp1* second() { return m_b; } \
 }; \
 
 #define DECL_OPR_2(Name, Opr) \
 template<typename T0, typename T1> constexpr auto Name##Func(T0 a, T1 b) { return a Opr b; } \
 DECL_OPR_CLASS_2(Name, Name##Func, "(", " " #Opr " ", ")") \
 template<typename T0, typename T1> \
-constexpr Name##PP<Var<T0>, Var<T1>> operator Opr(Var<T0>& a, Var<T1>& b) \
+constexpr Name<Var<T0>*, Var<T1>*> operator Opr(Var<T0>& a, Var<T1>& b) \
 { return {&a, &b}; } \
 template<typename T, typename A, typename B, \
          template<class...> class Exp> \
-constexpr Name##PV<Var<T>,Exp<A,B>> operator Opr(Var<T>& a, Exp<A,B> b) \
+constexpr Name<Var<T>*,Exp<A,B>> operator Opr(Var<T>& a, Exp<A,B> b) \
 { return {&a, b}; } \
 template<typename T, typename A, typename B, \
          template<class...> class Exp> \
-constexpr Name##VP<Exp<A,B>,Var<T>> operator Opr(Exp<A,B> a, Var<T>& b) \
+constexpr Name<Exp<A,B>,Var<T>*> operator Opr(Exp<A,B> a, Var<T>& b) \
 { return {a, &b}; } \
 template<typename A, typename B, \
          typename C, typename D, \
          template<class...> class Exp0, \
          template<class...> class Exp1> \
-constexpr Name##VV<Exp0<A,B>,Exp1<C,D>> operator Opr(Exp0<A,B> a, Exp1<C,D> b) \
+constexpr Name<Exp0<A,B>,Exp1<C,D>> operator Opr(Exp0<A,B> a, Exp1<C,D> b) \
 { return {a, b}; } \
 template<typename A, typename B, \
          template<class...> class Exp0, \
          typename T> \
-constexpr Name##VV<Exp0<A,B>,Const<T,DummyClass>> operator Opr(Exp0<A,B> a, T b) \
+constexpr Name<Exp0<A,B>,Const<T,DummyClass>> operator Opr(Exp0<A,B> a, T b) \
 { return {a, Const{b}}; } \
 template<typename A, typename B, \
          template<class...> class Exp0, \
          typename T> \
-constexpr Name##VV<Const<T,DummyClass>,Exp0<A,B>> operator Opr(T a, Exp0<A,B> b) \
+constexpr Name<Const<T,DummyClass>,Exp0<A,B>> operator Opr(T a, Exp0<A,B> b) \
 { return {Const{a}, b}; } \
 template<typename T0, typename T1> \
-constexpr Name##VP<Const<T0,DummyClass>,Var<T1>> operator Opr(T0 a, Var<T1>& b) \
+constexpr Name<Const<T0,DummyClass>,Var<T1>*> operator Opr(T0 a, Var<T1>& b) \
 { return {Const{a}, &b}; } \
 template<typename T0, typename T1> \
-constexpr Name##PV<Var<T0>,Const<T1,DummyClass>> operator Opr(Var<T0>& a, T1 b) \
+constexpr Name<Var<T0>*,Const<T1,DummyClass>> operator Opr(Var<T0>& a, T1 b) \
 { return {&a, Const{b}}; } \
 
 #define DECL_FUNC_2(Name, Func) \
 DECL_OPR_CLASS_2(Name, Func, #Func "( ", ", ", " )") \
 template<typename T0, typename T1> \
-constexpr Name##PP<Var<T0>, Var<T1>> Func(Var<T0>& a, Var<T1>& b) \
+constexpr Name<Var<T0>*, Var<T1>*> Func(Var<T0>& a, Var<T1>& b) \
 { return {&a, &b}; } \
 template<typename T, typename A, typename B, \
          template<class...> class Exp> \
-constexpr Name##PV<Var<T>,Exp<A,B>> Func(Var<T>& a, Exp<A,B> b) \
+constexpr Name<Var<T>*,Exp<A,B>> Func(Var<T>& a, Exp<A,B> b) \
 { return {&a, b}; } \
 template<typename T, typename A, typename B, \
          template<class...> class Exp> \
-constexpr Name##VP<Exp<A,B>,Var<T>> Func(Exp<A,B> a, Var<T>& b) \
+constexpr Name<Exp<A,B>,Var<T>*> Func(Exp<A,B> a, Var<T>& b) \
 { return {a, &b}; } \
 template<typename A, typename B, \
          typename C, typename D, \
          template<class...> class Exp0, \
          template<class...> class Exp1> \
-constexpr Name##VV<Exp0<A,B>,Exp1<C,D>> Func(Exp0<A,B> a, Exp1<C,D> b) \
+constexpr Name<Exp0<A,B>,Exp1<C,D>> Func(Exp0<A,B> a, Exp1<C,D> b) \
 { return {a, b}; } \
 template<typename A, typename B, \
          template<class...> class Exp0, \
          typename T> \
-constexpr Name##VV<Exp0<A,B>,Const<T,DummyClass>> Func(Exp0<A,B> a, T b) \
+constexpr Name<Exp0<A,B>,Const<T,DummyClass>> Func(Exp0<A,B> a, T b) \
 { return {a, Const{b}}; } \
 template<typename A, typename B, \
          template<class...> class Exp0, \
          typename T> \
-constexpr Name##VV<Const<T,DummyClass>,Exp0<A,B>> Func(T a, Exp0<A,B> b) \
+constexpr Name<Const<T,DummyClass>,Exp0<A,B>> Func(T a, Exp0<A,B> b) \
 { return {Const{a}, b}; } \
 template<typename T0, typename T1> \
-constexpr Name##VP<Const<T0,DummyClass>,Var<T1>> Func(T0 a, Var<T1>& b) \
+constexpr Name<Const<T0,DummyClass>,Var<T1>*> Func(T0 a, Var<T1>& b) \
 { return {Const{a}, &b}; } \
 template<typename T0, typename T1> \
-constexpr Name##PV<Var<T0>,Const<T1,DummyClass>> Func(Var<T0>& a, T1 b) \
+constexpr Name<Var<T0>*,Const<T1,DummyClass>> Func(Var<T0>& a, T1 b) \
 { return {&a, Const{b}}; } \
 
 #define DECL_OPR_1(Name, Opr) \
 template<typename T> constexpr auto Name##Func(T a) { return Opr a; } \
 DECL_OPR_CLASS_1(Name, Name##Func, "( " #Opr, " )") \
 template<typename T> \
-constexpr Name##P<Var<T>,DummyClass> operator Opr(Var<T>& a) \
+constexpr Name<Var<T>*,DummyClass> operator Opr(Var<T>& a) \
 { return {&a}; } \
 template<typename A, typename B, \
          template<class...> class Exp> \
-constexpr Name##V<Exp<A,B>,DummyClass> operator Opr(Exp<A,B> a) \
+constexpr Name<Exp<A,B>,DummyClass> operator Opr(Exp<A,B> a) \
 { return {a}; } \
 
 #define DECL_FUNC_1(Name, Func) \
 DECL_OPR_CLASS_1(Name, Func, #Func "( ", " )") \
 template<typename T> \
-constexpr Name##P<Var<T>,DummyClass> Func(Var<T>& a) \
+constexpr Name<Var<T>*,DummyClass> Func(Var<T>& a) \
 { return {&a}; } \
 template<typename A, typename B, \
          template<class...> class Exp> \
-constexpr Name##V<Exp<A,B>,DummyClass> Func(Exp<A,B> a) \
+constexpr Name<Exp<A,B>,DummyClass> Func(Exp<A,B> a) \
 { return {a}; } \
 
 DECL_OPR_2(Add,+)
@@ -303,6 +230,7 @@ DECL_OPR_2(Div,/)
 DECL_OPR_1(Neg,-)
 DECL_OPR_1(Pos,+)
 DECL_FUNC_1(AbsExp, Abs)
+DECL_FUNC_1(SqrtExp, Sqrt)
 DECL_FUNC_1(CosExp, Cos)
 DECL_FUNC_1(SinExp, Sin)
 DECL_FUNC_1(TanExp, Tan)
@@ -311,7 +239,50 @@ DECL_FUNC_1(ASinExp, ASin)
 DECL_FUNC_1(ATanExp, ATan)
 DECL_FUNC_2(ATan2Exp, ATan2)
 
-template<class T> constexpr auto $(T& v) { return v.val(); }
+/* Addition with ZeroExp */
+template<typename T0, typename T1>
+constexpr auto operator+(Var<T0>& a, ZeroExp<T1> b) { return a; }
+template<typename T0, typename T1>
+constexpr auto operator+(ZeroExp<T0> a, Var<T1>& b) { return b; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator+(Exp<A,B> a, ZeroExp<T> b) { return a; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator+(ZeroExp<T> a, Exp<A,B> b) { return b; }
+template<typename T0, typename T1>
+constexpr auto operator+(ZeroExp<T0> a, ZeroExp<T1> b) { return a; }
+
+/* Subtraction with ZeroExp */
+template<typename T0, typename T1>
+constexpr auto operator-(Var<T0>& a, ZeroExp<T1> b) { return a; }
+template<typename T0, typename T1>
+constexpr auto operator-(ZeroExp<T0> a, Var<T1>& b) { return -b; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator-(Exp<A,B> a, ZeroExp<T> b) { return a; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator-(ZeroExp<T> a, Exp<A,B> b) { return -b; }
+template<typename T0, typename T1>
+constexpr auto operator-(ZeroExp<T0> a, ZeroExp<T1> b) { return a; }
+
+/* Multiplication by ZeroExp */
+template<typename T0, typename T1>
+constexpr auto operator*(Var<T0>& a, ZeroExp<T1> b) { return b; }
+template<typename T0, typename T1>
+constexpr auto operator*(ZeroExp<T0> a, Var<T1>& b) { return a; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator*(Exp<A,B> a, ZeroExp<T> b) { return b; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator*(ZeroExp<T> a, Exp<A,B> b) { return a; }
+template<typename T0, typename T1>
+constexpr auto operator*(ZeroExp<T0> a, ZeroExp<T1> b) { return a; }
+
+/* Division where ZeroExp is a numerator */
+template<typename T0, typename T1>
+constexpr auto operator/(ZeroExp<T0> a, Var<T1>& b) { return a; }
+template<typename A, typename B, template<class...> class Exp, typename T>
+constexpr auto operator/(ZeroExp<T> a, Exp<A,B> b) { return a; }
+
+template<class T> constexpr auto $(T&& v) { return v.val(); }
+template<class T> constexpr auto $(T* v) { return v->val(); }
 
 } // namespace frogs
 
