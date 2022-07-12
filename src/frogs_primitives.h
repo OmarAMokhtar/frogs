472de08e6389141cc89eb41af9b22b511b94457c
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-13 00:45:39 +0200

    Update frogs_primitives.h

--- src/frogs_primitives.h
+++ src/frogs_primitives.h
@@ -8,25 +8,54 @@ namespace frogs
 {
 
 using Integer = unsigned long long;
-using Real = long double;
+using Real = double;
 using Str = std::string;
 
 template<class T> Str conv2str(T&& v) { return v.toString(); }
 template<class T> Str conv2str(T* v) { return v->toString(); }
-inline Str conv2str(double v) { return std::to_string(v); }
-inline Str conv2str(Real v) { return std::to_string(v); }
+Str conv2str(Real v);
+
 inline Str conv2str(Integer v) { return std::to_string(v); }
 inline Str conv2str(int v) { return std::to_string(v); }
 
-inline auto Abs(double v) { return v < 0 ? -v : v; }
 inline auto Abs(Real v) { return v < 0 ? -v : v; }
 inline auto Abs(Integer v) { return v < 0 ? -v : v; }
 inline auto Abs(int v) { return v < 0 ? -v : v; }
 
-inline auto Sqrt(double v) { return sqrt(v); }
 inline auto Sqrt(Real v) { return sqrt(v); }
 inline auto Sqrt(Integer v) { return sqrt(v); }
 inline auto Sqrt(int v) { return sqrt(v); }
+
+constexpr Real One(Real) { return 1.0; }
+constexpr Real Zero(Real) { return 0.0; }
+constexpr Integer One(Integer) { return 1; }
+constexpr Integer Zero(Integer) { return 0; }
+constexpr int One(int) { return 1; }
+constexpr int Zero(int) { return 0; }
+
+inline Str operator+(Str a, Str b)
+{
+    Str result = a;
+    result += b;
+    return result;
+}
+
+inline Str operator+(Str a, const char* b)
+{
+    Str result = a;
+    result += b;
+    return result;
+}
+
+inline Str operator+(const char* a, Str b)
+{
+    Str result = a;
+    result += b;
+    return result;
+}
+
+template<typename T>
+constexpr T& fwd(T&& v) { return std::forward<T&>(v); };
 
 } // namespace frogs
 
