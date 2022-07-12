1bde170afa48dcd8f835624a3c9f558f072d7955
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-13 01:02:31 +0200

    Update matrix_example.cpp

--- example/matrix_example.cpp
+++ example/matrix_example.cpp
@@ -19,9 +19,14 @@ int32_t main()
     cout << "Shape Before Transformation:" << endl;
     cout << shape << endl;
 
+    /* The transformation is done in the same order
+     * as the code. Usually we scale/rotate first
+     * and then translate.
+     */
     Mat4 mat;
     mat.rotate(45_deg, 0, 0, 1);
     mat.scale(2.0);
+    mat.translate(50_m, 50_m);
 
     cout << "Shape After Transformation:" << endl;
     cout << mat * shape << endl;
