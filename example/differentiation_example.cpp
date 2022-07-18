ba0d2bcf48cf6c2ee07bd18d461818e27d98d7a5
Omar Mokhtar <45882058+OmarAMokhtar@users.noreply.github.com>
2022-07-18 17:06:13 +0200

    Update differentiation_example.cpp

--- example/differentiation_example.cpp
+++ example/differentiation_example.cpp
@@ -13,7 +13,7 @@ int32_t main()
     /* Let's create the classic equation of getting the distance at a certain time.
      * Our variable is of type Time and initially at zero.
      */
-    auto t = Var{0_sec};
+    auto t = Var{0_sec, "myTime"};
     /* Then the 2nd degree equation: */
     auto distance = 0.2_mps2*t*t + 10_mps*t + 20_m;
 
@@ -24,11 +24,14 @@ int32_t main()
     auto acceleration = Diff(velocity, t);
 
     /* Let's print the formulas first: */
+    cout << "The formulas are:" << endl;
     cout << "distance = " << distance << endl;
     cout << "velocity = " << velocity << endl;
     cout << "acceleration = " << acceleration << endl;
+    cout << endl;
 
     /* Now let's evaluate all of them at different values of t */
+    cout << "Printing several values:" << endl;
     for (auto currTime : Range(5_sec))
     {
         t = currTime;
