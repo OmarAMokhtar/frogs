953c086ed6523d39279a7c4aeb13620aa0f1b034
Omar Mokhtar <omar.ali.mokhtar@gmail.com>
2022-12-27 14:41:37 +0100

    Fixing a bug with using Range() as an iterator for a for-loop when it's a floating point

--- src/frogs_utils.h
+++ src/frogs_utils.h
@@ -45,12 +45,28 @@ public:
     private:
         T m_val;
         T m_step;
+        bool m_isEnd;
+        bool m_isBegin;
 
     public:
-        Iter(T val, T step) : m_val{val}, m_step{step} {}
-        Iter(Iter& other) : m_val{other.m_val}, m_step{other.m_step} {}
-        Iter(Iter&& other) : m_val{other.m_val}, m_step{other.m_step} {}
-        
+        Iter(T val, T step, bool end = false, bool begin = false)
+            : m_val{val}
+            , m_step{step}
+            , m_isEnd{end}
+            , m_isBegin{begin} {}
+
+        Iter(Iter& other)
+            : m_val{other.m_val}
+            , m_step{other.m_step}
+            , m_isEnd{other.m_isEnd}
+            , m_isBegin{other.m_isBegin} {}
+
+        Iter(Iter&& other)
+            : m_val{other.m_val}
+            , m_step{other.m_step}
+            , m_isEnd{other.m_isEnd}
+            , m_isBegin{other.m_isBegin} {}
+
         Iter& operator=(Iter&& other)
         {
             m_val = other.m_val;
@@ -68,6 +84,7 @@ public:
         Iter& operator++()
         {
             m_val += m_step;
+            m_isBegin = false;
             return *this;
         }
 
@@ -75,24 +92,43 @@ public:
         {
             Iter tmp = *this;
             m_val += m_step;
+            m_isBegin = false;
             return tmp;
         }
 
         T& operator*() { return m_val; }
         T* operator->() { return &m_val; }
 
-        friend bool operator==(Iter a, Iter b) { return a.m_val == b.m_val; }
-        friend bool operator!=(Iter a, Iter b) { return a.m_val != b.m_val; }
+        friend bool operator==(Iter a, Iter b)
+        {
+            if (a.m_isBegin && b.m_val <= a.m_val)
+                return true;
+            if (b.m_isBegin && a.m_val <= b.m_val)
+                return true;
+            if (a.m_isEnd && b.m_val >= a.m_val)
+                return true;
+            if (b.m_isEnd && a.m_val >= b.m_val)
+                return true;
+            return a.m_val == b.m_val;
+        }
+        friend bool operator!=(Iter a, Iter b) { return !(a == b); }
     };
 
-    Range::Iter begin() { return {m_min, m_step}; }
-    Range::Iter end() { return {m_max, m_step}; }
+    Range::Iter begin() { return {m_min, m_step, false, true}; }
+    Range::Iter end() { return {m_max, m_step, true, false}; }
 
     friend constexpr bool In(T v, Range range)
     {
         auto min = std::min(range.m_max, range.m_min);
         auto max = std::max(range.m_max, range.m_min);
         return (v <= max && v >= min);
+    }
+
+    friend constexpr bool Overlap(Range a, Range b)
+    {
+        if (In(a.m_min, b) || In(a.m_max, b) || In(b.m_min, a) || In(b.m_max, a))
+            return true;
+        return false;
     }
 };
 
